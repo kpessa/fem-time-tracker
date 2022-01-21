7f8381359f0657fb02e8f34498ec153ae90a5740
Kurt Pessa <kpessa@gmail.com>
2022-01-20 21:38:42 -0500

    fixed hr vs hrs logic

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -3,9 +3,13 @@ import React from 'react';
 export default function CardDetails({ time, timeframes }) {
 	return (
 		<div className={`flex md:flex-col md:items-start items-center justify-between w-full`}>
-			<h1 className={`font-sans text-[32px] md:text-[56px] tracking-tight font-light`}>{timeframes[time]?.current}hrs</h1>
+			<h1 className={`font-sans text-[32px] md:text-[56px] tracking-tight font-light`}>
+				{timeframes[time]?.current}
+				{timeframes[time]?.current !== 1 ? 'hrs' : 'hr'}
+			</h1>
 			<p className="font-sans text-[15px] text-updated">
-				{time === 'monthly' ? 'Last Month' : time === 'weekly' ? 'Last Week' : 'Yesterday'} - {timeframes[time]?.previous}hrs
+				{time === 'monthly' ? 'Last Month' : time === 'weekly' ? 'Last Week' : 'Yesterday'} - {timeframes[time]?.previous}
+				{timeframes[time]?.previous !== 1 ? 'hrs' : 'hr'}
 			</p>
 		</div>
 	);
